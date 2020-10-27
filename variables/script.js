bcfce6dcef4b4f51d049a0a330471a829d4fc11e
Yedil Akhmetkarimov <edil.eakhmetkarimov@gmail.com>
2020-10-27 19:07:40 +0600

    first commit, variables

---
+++ variables/script.js
@@ -0,0 +1,69 @@
+// alert("Hello World!");
+console.log("Hello World! console")
+document.write("Hello world! document")
+
+// var, let, const 
+
+// var a = 20
+// var b = 40
+
+// a = 90
+
+// console.log(c)
+
+// var c = a + b
+
+// console.log(c)
+
+
+let letVar = 20
+
+console.log(letVar)
+
+
+const maxElem = 200
+
+console.log(maxElem)
+
+
+let num1 = 40
+let num2 = 60
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+let s = 'Hello wordl'
+
+console.log('Pow', Math.pow(5, 2))
+console.log('Sqrt', Math.sqrt(81))
+console.log('Round', Math.round(5.2))
+console.log('Ceil', Math.ceil(5.1))
+console.log('Floor', Math.floor(5.9))
+
+let resultSqut = Math.sqrt(144)
+let resultPow = Math.pow(resultSqut, 3)
+
+
+let a = 4
+let b = 3
+
+// let c = Math.sqrt((a*a) + (b*b))
+let c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+
+console.log(c)
+
+
+let seconds = 60
+let minute = 60
+let hour = 24
+let resultSeconds = seconds * minute * hour
+console.log(resultSeconds)
+
+
+
+
+
+
